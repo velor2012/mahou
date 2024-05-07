f14c7c2fe26ab37e156df0d42689b47bd141b752
velor2012 <38395332+velor2012@users.noreply.github.com>
2024-05-07 20:05:27 +0800

    fix bug

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 设置自定义的退出状态
+CUSTOM_EXIT_STATUS=0
+
+# 运行命令并获取其退出状态
+timeout 10s pnpm start
+exit_status=$?
+
+# 检查是否由于超时而退出
+if [ $exit_status -eq 124 ]; then
+  # 手动设置退出状态为我们的自定义值
+  exit $CUSTOM_EXIT_STATUS
+else
+  # 保持原来的退出状态
+  exit $exit_status
+fi
